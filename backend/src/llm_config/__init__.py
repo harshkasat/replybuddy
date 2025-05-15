3796f81741e69c1cc9329f45d301ad776bf043b3
Zedmat <104870914+harshkasat@users.noreply.github.com>
2025-05-16 04:52:37 +0530

    initial backend

---
+++ backend/src/llm_config/__init__.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+SAFE_SETTINGS = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE",
+    },
+]
+
+genai_api_key = os.getenv('GEMINI_API_KEY')
+os.environ['GOOGLE_API_KEY'] = os.getenv('GEMINI_API_KEY')
+if genai_api_key is None:
+    raise ValueError("Missing GEMINI_API_KEY environment variable")
---
+++ backend/src/llm_config/config.py
@@ -0,0 +1,35 @@
+import logging
+from google import genai
+from google.genai import types as google_config
+from llm_config import genai_api_key, SAFE_SETTINGS
+
+
+# Initialize the API client
+class LlmClient:
+    def __init__(self, prompt_instruction):
+        try:
+            self.client = genai.Client(api_key=genai_api_key)
+            self.config = google_config.GenerateContentConfig(
+                system_instruction=prompt_instruction,
+                safety_settings=SAFE_SETTINGS,
+                response_mime_type="application/json",
+            )
+            logging.info("LLM component initialized successfully")
+        except Exception as e:
+            logging.error(f"Failed to configure LLM: {e}")
+
+    def generate_content(self, content):
+        try:
+            response = self.client.models.generate_content(
+                model="gemini-2.0-flash",
+                contents=content,
+                config=self.config,
+            )
+            logging.info("LLM response successfully generated")
+            if response.text is None:
+                logging.error("LLM response is None")
+                return None
+            return response
+        except Exception as e:
+            logging.error(f"Failed to generate content: {e}")
+            return None
